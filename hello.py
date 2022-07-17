6caf49ee44ed5a7ac7a02fa0e3a10d959371ebd4
coryphoenixxx <coryphoenixxx@gmail.com>
2022-07-18 01:22:50 +0700

    fix hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 def app(env, start_response):
-    params = '\n'.join(env['QUERY_STRING'].split('&'))
+    params = [bytes(i + '\n', 'ascii') for i in env['QUERY_STRING'].split('&')]
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    return bytes(params)
+    return params
